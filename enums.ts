37fee133e4d42ca88443698dca8e48f568b671ed
Pascal Eberhard <35179162+paeber@users.noreply.github.com>
2022-03-26 11:06:25 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, enums.ts

---
+++ enums.ts
@@ -0,0 +1,20 @@
+// Enum defines
+
+enum MCP_Regs {
+    IODIR = 0x01,
+    DEFVAL = 0x08,
+    GPPU = 0x06,
+    GPIO = 0x09
+}
+
+
+enum MCP_Pins {
+    RAK_RST = 0x01,
+    RAK_LED = 0x02,
+    OC1     = 0x04,
+    OC2     = 0x08,
+    GP4     = 0x10,
+    GP5     = 0x20, 
+    GP6     = 0x40,
+    USR_LED = 0x80
+}
--- main.ts
+++ main.ts
@@ -1,3 +1,49 @@
+/**
+ * IoT-Wuerfel 
+ * GBS St. Gallen, 2022
+ */
+
+//% color="#00796b" icon="\uf1eb"
+namespace MCP23008 {
+    let i2c_addr = 0x20
+
+    //% blockId="MCP_I2C_Write"
+    //% block="MCP I2C Write to %register Data %value"
+    //% advanced=false
+    export function i2c_write(register: MCP_Regs, value: number) {
+        pins.i2cWriteNumber(i2c_addr, register, NumberFormat.UInt8LE, true)
+        pins.i2cWriteNumber(i2c_addr, value, NumberFormat.UInt8LE, false)
+    }
+
+
+    //% blockId="MCP_I2C_Read"
+    //% block="MCP read register %register"
+    //% advanced=false
+    export function i2c_read(register: MCP_Regs) {
+        pins.i2cWriteNumber(i2c_addr, register, NumberFormat.UInt8LE, true)
+        return pins.i2cReadNumber(i2c_addr, NumberFormat.UInt8LE, false)
+    }
+
+    //% blockId="MCP_Pin_Set"
+    //% block="MCP set pin %pin to %value"
+    //% advanced=false
+    export function pin_set(pin: MCP_Pins, value: number) {
+        let pinvalue = i2c_read(MCP_Regs.GPIO)
+        if (value) {
+            value = pinvalue | pin
+        }
+        else {
+            value = pinvalue & ~pin
+        }
+        pins.i2cWriteNumber(i2c_addr, MCP_Regs.GPIO, NumberFormat.UInt8LE, true)
+        pins.i2cWriteNumber(i2c_addr, value, NumberFormat.UInt8LE, false)
+        return value
+    }
+
+
+}
+
+
 basic.forever(function () {
 	
 })
